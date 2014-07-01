454436f7a269052eb8016e161427fd12e9b0db99
iksong <solita.song@gmail.com>
2014-07-01 13:54:31 -0500

    test no. 2

--- TestingGit/AppDelegate.m
+++ TestingGit/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"testing..");
+    NSLog(@"testing.. one more...");
     return YES;
 }
 							
